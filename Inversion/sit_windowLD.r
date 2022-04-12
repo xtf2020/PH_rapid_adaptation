c9b9c8bcef8857b0375f586fcbb378405e22da99
xtf2020 <70941658+xtf2020@users.noreply.github.com>
2022-04-12 08:56:19 +0800

    Update sit_windowLD.r

--- Inversion/sit_windowLD.r
+++ Inversion/sit_windowLD.r
@@ -12,3 +12,12 @@ d1$w1 <- as.numeric(d1$w1)
 d1$w2 <- as.numeric(d1$w2)
 d1 <- arrange(d1,w1,w2)
 write.table(d1,file="lg21w_com",quote=F,row.names=F,sep="\t")
+##plot window LD results
+#d1$w1 <- d1$w1*50000/1000000
+#d1$w2 <- d1$w2*50000/1000000
+#p<-ggplot(d,aes(x=w1,y=w2)) +
+#     theme_bw() +
+#     geom_tile(aes(fill=percentile)) + scale_fill_continuous(type = "viridis" )+
+#     labs(title="LG26",x="Chromosome 26 (Mbp)",y="Chromosome 26 (Mbp)") +
+#     theme(plot.title = element_text(size=30,hjust = 0.5),panel.border = element_blank(),panel.grid.major = element_blank(), panel.grid.minor = element_blank(),axis.line = element_line(colour = "black"))
+#ggsave(filename = "lg26.svg",plot = p)
